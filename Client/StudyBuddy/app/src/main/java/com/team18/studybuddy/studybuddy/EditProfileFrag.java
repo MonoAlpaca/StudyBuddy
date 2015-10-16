176a75df96e807370d877aa60b8f710ac92cb947
Weskimo14 <zwezensk@purdue.edu>
2015-10-16 18:21:02 -0400

    Updated UI
    Mostly done

---
+++ Client/StudyBuddy/app/src/main/java/com/team18/studybuddy/studybuddy/EditProfileFrag.java
@@ -0,0 +1,45 @@
+package com.team18.studybuddy.studybuddy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Zeb on 10/15/2015.
+ */
+public class EditProfileFrag extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static EditProfileFrag newInstance(int sectionNumber) {
+        EditProfileFrag fragment = new EditProfileFrag();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView;
+        rootView = inflater.inflate(R.layout.fragment_editprofile, container, false);
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(
+                getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+
+}
