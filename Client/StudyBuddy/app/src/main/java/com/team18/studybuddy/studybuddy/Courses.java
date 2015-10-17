9f25002693549399118f14f207133ecc14b2ca5a
Bor Shiuan chen <chen1370@purdue.edu>
2015-10-16 23:09:01 -0400

    Course Search
    Now accessible from profile rather than classes
    Added turning thing
    now dropdown shows after CS

--- Client/StudyBuddy/app/src/main/java/com/team18/studybuddy/studybuddy/Courses.java
+++ Client/StudyBuddy/app/src/main/java/com/team18/studybuddy/studybuddy/Courses.java
@@ -3,6 +3,7 @@ package com.team18.studybuddy.studybuddy;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,19 +28,36 @@ public class Courses extends Activity {
         setContentView(R.layout.activity_courses);
 
 
-
-
-        //
-        final AutoCompleteTextView subjectTextView = (AutoCompleteTextView) findViewById(R.id.subjectComplete);
-        subjectTextView.setAdapter(new SuggestionAdapter(this, R.layout.simple_dropdown_item_2line, R.id.text1));
-
+        final DelayAutoCompleteTextView subjectTextView = (DelayAutoCompleteTextView) findViewById(R.id.subjectComplete);
+        subjectTextView.setThreshold(2);
+        subjectTextView.setAdapter(new SuggestionAdapter(this, R.layout.simple_dropdown_item_2line, R.id.text1)); // 'this' is Activity instance
+        subjectTextView.setLoadingIndicator(
+                (android.widget.ProgressBar) findViewById(R.id.pb_loading_indicator));
         subjectTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 SuggestGetSet courseName = (SuggestGetSet) adapterView.getItemAtPosition(position);
-                subjectTextView.setText((courseName.getId()));
+                if (JsonParse.isAlpha(courseName.getId())) {
+                    subjectTextView.setText((courseName.getId()) + " ");
+                    new Handler().postDelayed(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            subjectTextView.showDropDown();
+                        }
+                    }, 1000);
+                } else {
+                    subjectTextView.setText((courseName.getId()));
+
+                }
+                subjectTextView.setSelection(subjectTextView.getText().length());
             }
         });
+        //
+        /*final AutoCompleteTextView subjectTextView = (AutoCompleteTextView) findViewById(R.id.subjectComplete);
+        subjectTextView.setAdapter(new SuggestionAdapter(this, R.layout.simple_dropdown_item_2line, R.id.text1));*/
+
+
     }
 
 
