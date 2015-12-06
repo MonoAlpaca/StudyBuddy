787d2af9d2f6ac1aade665573b3f6b00a095272d
Bor Shiuan chen <chen1370@purdue.edu>
2015-12-05 19:56:56 -0500

    Fonts work

--- Client/StudyBuddy/app/src/main/java/com/team18/studybuddy/studybuddy/CASClient.java
+++ Client/StudyBuddy/app/src/main/java/com/team18/studybuddy/studybuddy/CASClient.java
@@ -32,8 +32,7 @@ import ch.boye.httpclientandroidlib.impl.client.HttpClientBuilder;
 public class CASClient extends Activity {
 
     protected void onCreate(Bundle savedInstanceState) {
-        TypefaceUtil.overrideFont(getApplicationContext(), "DEFAULT", "fonts/OpenSans-Light.ttf");
-        TypefaceUtil.overrideFont(getApplicationContext(), "DEFAULT_BOLD", "fonts/OpenSans-Bold.ttf");// font from assets: "assets/fonts/Roboto-Regular.ttf
+        TypefaceUtil.overrideFont(getApplicationContext(), "SERIF", "fonts/OpenSans-Light.ttf");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.authentication);
         HttpClient httpClient = HttpClientBuilder.create().setRedirectStrategy(new DefaultRedirectStrategy()).build();
